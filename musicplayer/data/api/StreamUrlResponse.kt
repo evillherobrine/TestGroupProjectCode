eac7970f9ce1828bd0e81fc2300b4f3fa42e5d6a
evillherobrine <giang2ans1@gmail.com>
2026-01-02 08:50:44 +0700

    navigation rail

--- musicplayer/data/api/StreamUrlResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.example.musicplayer.data.api
-
-import com.google.gson.annotations.SerializedName
-
-data class StreamUrlResponse(
-    @SerializedName("stream_url")
-    val streamUrl: String
-)
--- musicplayer/domain/usecase/FavoriteUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.musicplayer.domain.usecase
-
-import com.example.musicplayer.domain.model.Song
-import com.example.musicplayer.domain.repository.FavoriteRepository
-import kotlinx.coroutines.flow.Flow
-
-class FavoriteUseCase(favoriteRepository: FavoriteRepository) {
-    val favoriteSongs: Flow<List<Song>> = favoriteRepository.favoriteSongs
-}
